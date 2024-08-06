ffdd820e2e3e8143246539ddcf4683e137a8cb6f
adrian92612 <adrian.villamin0612@gmail.com>
2024-08-06 14:49:38 +0800

    initial home page added

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,15 @@
+import Link from "next/link";
+
 const HomePage = () => {
-  return <div>Home Page</div>;
+  return (
+    <div className="h-full flex flex-col justify-center items-center gap-2">
+      <h1>Task Tracker</h1>
+      <p>A very simple and straightforward todo app.</p>
+      <Link href="/login" className="px-5 py-1 bg-slate-300">
+        <span>Log In</span>
+      </Link>
+    </div>
+  );
 };
 
 export default HomePage;
